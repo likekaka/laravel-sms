ebb6e8233526c1dde6b7927491844f6bae6fd2d5
RichardFans <492241727@qq.com>
2017-06-16 20:44:08 +0800

    批量发短信

--- src/Toplan/LaravelSms/agents/LuosimaoAgent.php
+++ src/Toplan/LaravelSms/agents/LuosimaoAgent.php
@@ -8,16 +8,17 @@ class LuosimaoAgent extends Agent
         $this->sendContentSms($to, $content);
     }
 
+
     public function sendContentSms($to, $content)
     {
         //因为Luosimao的签名必须放在最后，所以发送前需要检测签名位置
         //如果不是在最后，则调整至最后
         $content = trim($content);
-        if ($content && ! preg_match('/】$/', $content)) {
+        if ($content && !preg_match('/】$/', $content)) {
             preg_match('/【([0-9a-zA-Z\W]+)】/', $content, $matches);
             $content = str_replace($matches[0], '', $content) . $matches[0];
         }
-       
+
         if (strpos($to, ",") === false) {
             $url = 'https://sms-api.luosimao.com/v1/send.json';
             $optData = [
@@ -29,25 +30,10 @@ class LuosimaoAgent extends Agent
             $optData = [
                 'mobile_list'  => $to,
                 'message' => $content,
-            ];               
+            ];
         }
 
-        $apikey = $this->apikey;
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "$url");
-
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_HEADER, FALSE);
-
-        curl_setopt($ch, CURLOPT_HTTPAUTH , CURLAUTH_BASIC);
-        curl_setopt($ch, CURLOPT_USERPWD  , 'api:key-' . $apikey);
-
-        curl_setopt($ch, CURLOPT_POST, TRUE);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $optData);
-
-        $res = curl_exec( $ch );
-        curl_close( $ch );
+        $res = $this->LuosimaoCurl($url, $optData, $this->apikey);
 
         $data = json_decode($res, true);
         if ($data['error'] == 0) {
@@ -57,6 +43,27 @@ class LuosimaoAgent extends Agent
         $this->result['code'] = $data['error'];
     }
 
+    protected function LuosimaoCurl($url, $optData, $apikey)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "$url");
+
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_USERPWD, 'api:key-' . $apikey);
+
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $optData);
+
+        $res = curl_exec($ch);
+        curl_close($ch);
+
+        return $res;
+    }
+    
     public function sendTemplateSms($tempId, $to, Array $data)
     {
         return null;
